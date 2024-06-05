2a1d3f93505e095a4ca0a3b881b52f926036dd04
Valeryio <tommagoma23@gmail.com>
2024-06-05 08:56:37 +0000

    Initialising the header file

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - create a binary tree node
+ * 
+ * @parent: (binary_tree), the parent tree
+ * @value: (int), the value of the new node
+ * Description: This function creates a new node
+ * of a binary tree
+ * Return: A node
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+}
