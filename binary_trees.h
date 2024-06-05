83ddbbcb3ba0dcd6d134407e61bdc1ac080f3ae1
Valeryio <tommagoma23@gmail.com>
2024-06-05 14:26:25 +0000

    commit on the task

--- binary_trees.h
+++ binary_trees.h
@@ -18,3 +18,10 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+
+/*
+ static size_t _height(const binary_tree_t *tree);
+static int print_t(const binary_tree_t *tree, int offset, int depth, char **s)
+*/
+void binary_tree_print(const binary_tree_t *tree);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
